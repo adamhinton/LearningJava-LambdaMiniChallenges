c898f0c6cb822affdc610010f2e721a2ac86eb34
adamd <adam.d.hinton@gmail.com>
2024-03-14 22:36:54 -0400

    second solution

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,6 +1,8 @@
 package dev.lpa;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +12,31 @@ public class Main {
            for(String part : parts){
                System.out.println(part);
            }
+        });
+
+        Consumer<String> printWordsForEach = (sentence ->{
+            String[] parts = sentence.split(" ");
+            Arrays.asList(parts).forEach(s ->System.out.println(s));
+        });
+
+        printWordsForEach.accept("Let's split this up into an array");
+
+
+        Consumer<String> printWordsConcise = (sentence ->{
+          Arrays.asList(sentence.split(" ")).forEach(s -> System.out.println(s));
+        });
+
+        printWordsConcise.accept("Let's split this up into an array");
+
+
+         Function<String, String> everySecondChar = (source ->{
+            StringBuilder returnVal = new StringBuilder();
+            for(int i=0; i< source.length(); i++){
+                if(i %2 ==1){
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
         });
 
     }
